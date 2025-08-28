92d6aaa5cb993f214d445f1a870c35c74592d83a
static_knowledge <prakashsk1210@gmail.com>
2025-08-28 18:53:06 +0530

    delete cursor

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import SplashCursor from './components/SplashCursor.tsx'; // 👈 Import your cursor
-import './index.css';
+import { createRoot } from 'react-dom/client' 
+import App from './App.tsx' 
+import './index.css' 
 
-createRoot(document.getElementById("root")!).render(
-  <div className="relative">
-    <SplashCursor /> {/*  cursor animation */}
-    <App />
-  </div>
-);
+createRoot(document.getElementById("root")!).render(<App />);
